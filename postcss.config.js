9d8a6201518d4a832df1925ed62651a0759f40c1
Rajiv <1998irajeev@gmail.com>
2021-04-18 19:10:12 +0530

    tailwind test push 5

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  plugins: [
-    "postcss-preset-env",
-    "tailwindcss",
-    "tailwind.config.js",
-    "autoprefixer"
-  ]
+  plugins: ["postcss-preset-env", "tailwindcss", "autoprefixer"]
 }
