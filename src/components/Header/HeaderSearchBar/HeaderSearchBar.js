527e7ccbee91f6693b5b077acde81984beef4d7c
Rajiv <1998irajeev@gmail.com>
2021-06-11 16:14:29 +0530

    custom debounce hook added

--- src/components/Header/HeaderSearchBar/HeaderSearchBar.js
+++ src/components/Header/HeaderSearchBar/HeaderSearchBar.js
@@ -1,18 +1,28 @@
 import clsx from "clsx"
+import useDebouncedEffect from "hooks"
 import PropTypes from "prop-types"
+import { useState } from "react"
 import { FaSearch } from "react-icons/fa"
 import "./headerSearchBar.scss"
 
 const HeaderSearchBar = (props) => {
   const { className = "", ...restProps } = props
+
+  const [value, setValue] = useState("")
+
+  useDebouncedEffect(() => console.log(value), [value], 500)
   return (
-    <form className={clsx("headerSearchBarCotnainer", className)}>
+    <form
+      {...restProps}
+      className={clsx("headerSearchBarCotnainer", className)}
+    >
       <label htmlFor='header-search-bar'>
         <input
           type='text'
           name='query'
           // eslint-disable-next-line jsx-a11y/tabindex-no-positive
           tabIndex='1'
+          onChange={(e) => setValue(e.target.value)}
           autoCorrect='off'
           autofill='off'
           autoComplete='off'
