e5ea29bc95d4f69e6a8c987d4a577571d7b0be38
Rajiv <1998irajeev@gmail.com>
2021-06-13 19:45:40 +0530

    readme description added

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# WatchTime
+
+https://watch-time.netlify.app
+
+A react app for showing upcoming movies and tv shows in a minimal way. 
+
+Project is configured from scratch using wepack and babel configuration for react.
+
+_This project is still under construction (In Development)_
+
+
+
+
+
+## Tech Stack
+
+**Client/UI:** React, Redux, Redux-Thunk, TailwindCSS, SCSS, Immer
+
+**Data:** [TMDB](https://developers.themoviedb.org/3) Api service is used for fetching the data
+
+  
+## Lessons Learned
+
+- Creating weback and babel configuration from scratch for working of react and react-dom.
+- Creating Hight order components efficiently
--- src/components/SingleMediaPage/SingleMediaPage.js
+++ src/components/SingleMediaPage/SingleMediaPage.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-prototype-builtins */
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useLocation, useParams } from "react-router-dom"
 import { fetchSingleMovieData } from "appRedux/thunks/movies/actions"
 import { fetchSingleTvShowData } from "appRedux/thunks/tv/actions"
 import tmdbContants from "constants/tmdbContants"
 import ContentLayoutWrapper from "containers/ContentLayoutWrapper"
 import RingRating from "CustomComponents/RingRating/RingRating"
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useLocation, useParams } from "react-router-dom"
+import "./singleMediaPage.scss"
 
 const SingleMediaPage = () => {
   const dispatch = useDispatch()
@@ -37,84 +38,89 @@ const SingleMediaPage = () => {
   return (
     <ContentLayoutWrapper>
       {currentMediaData && (
-        <div className='grid grid-cols-8 gap-2 md:gap-4 text-white'>
-          <div className='col-span-2 rounded'>
-            <img
-              className='rounded'
-              src={`${tmdbContants.posterPath}${currentMediaData.poster_path}`}
-              alt=''
-            />
-          </div>
-          <div className='col-span-6'>
-            <h1 className='font-medium text-3xl pt-4'>
-              {currentMediaData?.title || currentMediaData?.name}
-              <span className='font-light opacity-80 ml-2'>
-                (
-                {new Date(
-                  currentMediaData?.release_date ||
-                    currentMediaData?.first_air_date
-                ).getFullYear()}
-                )
-              </span>
-            </h1>
-            <h4 className='text-xl opacity-80'>{currentMediaData?.tagline}</h4>
-            <div className='mt-4 flex items-center gap-2'>
-              {currentMediaData?.hasOwnProperty("adult") && (
-                <>
-                  <span className='rounded border border-white border-opacity-60 py-1 px-2'>
-                    {currentMediaData?.adult ? "A" : "U"}
-                  </span>
-                  &#8226;
-                </>
-              )}
-              {currentMediaData?.vote_average && (
-                <>
-                  <RingRating
-                    title='Rating'
-                    rating={currentMediaData?.vote_average ?? 0}
-                  />
-                  &#8226;
-                </>
-              )}
-              {currentMediaData?.release_date && (
-                <>
-                  <span title='Release Date'>
-                    {new Date(currentMediaData?.release_date)
-                      ?.toDateString()
-                      ?.substring(4)}
-                  </span>
-                  &#8226;
-                </>
-              )}
-              {currentMediaData?.runtime && (
-                <>
-                  <span>{currentMediaData?.runtime} min</span>
-                  &#8226;
-                </>
-              )}
-              {currentMediaData?.original_language && (
-                <>
-                  <span className='uppercase' title='Original Language'>
-                    {currentMediaData?.original_language}
-                  </span>
-                </>
-              )}
-              {currentMediaData?.number_of_seasons && (
-                <>
-                  &#8226;
-                  <span className='capitalize' title='Seasons'>
-                    {currentMediaData?.number_of_seasons}{" "}
-                    {`${
-                      currentMediaData?.number_of_seasons > 1
-                        ? "Seasons"
-                        : "Season"
-                    }`}
-                  </span>
-                </>
-              )}
+        <div>
+          <div className='grid grid-cols-8 gap-2 md:gap-4 text-white relative'>
+            <div className='bg-wrapper md:-left-12 -left-2' />
+            <div className='col-span-2 rounded'>
+              <img
+                className='rounded'
+                src={`${tmdbContants.posterPath}${currentMediaData.poster_path}`}
+                alt=''
+              />
             </div>
+            <div className='col-span-6'>
+              <h1 className='font-medium text-3xl pt-4'>
+                {currentMediaData?.title || currentMediaData?.name}
+                <span className='font-light opacity-80 ml-2'>
+                  (
+                  {new Date(
+                    currentMediaData?.release_date ||
+                      currentMediaData?.first_air_date
+                  ).getFullYear()}
+                  )
+                </span>
+              </h1>
+              <h4 className='text-xl opacity-80'>
+                {currentMediaData?.tagline}
+              </h4>
+              <div className='mt-4 flex items-center gap-2'>
+                {currentMediaData?.hasOwnProperty("adult") && (
+                  <>
+                    <span className='rounded border border-white border-opacity-60 py-1 px-2'>
+                      {currentMediaData?.adult ? "A" : "U"}
+                    </span>
+                    &#8226;
+                  </>
+                )}
+                {currentMediaData?.vote_average && (
+                  <>
+                    <RingRating
+                      title='Rating'
+                      rating={currentMediaData?.vote_average ?? 0}
+                    />
+                    &#8226;
+                  </>
+                )}
+                {currentMediaData?.release_date && (
+                  <>
+                    <span title='Release Date'>
+                      {new Date(currentMediaData?.release_date)
+                        ?.toDateString()
+                        ?.substring(4)}
+                    </span>
+                    &#8226;
+                  </>
+                )}
+                {currentMediaData?.runtime && (
+                  <>
+                    <span>{currentMediaData?.runtime} min</span>
+                    &#8226;
+                  </>
+                )}
+                {currentMediaData?.original_language && (
+                  <>
+                    <span className='uppercase' title='Original Language'>
+                      {currentMediaData?.original_language}
+                    </span>
+                  </>
+                )}
+                {currentMediaData?.number_of_seasons && (
+                  <>
+                    &#8226;
+                    <span className='capitalize' title='Seasons'>
+                      {currentMediaData?.number_of_seasons}{" "}
+                      {`${
+                        currentMediaData?.number_of_seasons > 1
+                          ? "Seasons"
+                          : "Season"
+                      }`}
+                    </span>
+                  </>
+                )}
+              </div>
 
-            <p className='mt-4 opacity-80'>{currentMediaData?.overview}</p>
+              <p className='mt-4 opacity-80'>{currentMediaData?.overview}</p>
+            </div>
           </div>
         </div>
       )}
